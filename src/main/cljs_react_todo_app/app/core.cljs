79b3ff3730a063403508a467066331d1c377070a
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2021-10-19 11:43:37 -0300

    Add app state atom

--- src/main/cljs_react_todo_app/app/core.cljs
+++ src/main/cljs_react_todo_app/app/core.cljs
@@ -1,5 +1,18 @@
 (ns cljs-react-todo-app.app.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+;; ----- App State -----
+
+(def initial-todos {1 {:id 1, :title "Do laundry", :done false}
+                    3 {:id 3, :title "Buy groceries", :done false}
+                    2 {:id 2, :title "Wash dishes", :done true}})
+
+(def initial-todos-sorted (into (sorted-map) initial-todos))
+
+(defonce todos (r/atom initial-todos-sorted))
+
+;; ----- Views -----
 
 (defn todo-input []
   [:input {:class "new-todo"
@@ -28,6 +41,8 @@
      [footer-controls]]]
    [:footer.info
     [:p "Footer info"]]])
+
+;; ----- Render -----
 
 (defn render []
   (rdom/render [app] (.getElementById js/document "root")))
